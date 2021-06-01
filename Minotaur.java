6c75837848ec4ad952f99ff5a80f914a5f4b9f99
SnakeMeister89 <84979838+SnakeMeister89@users.noreply.github.com>
2021-05-31 20:50:40 -0400

    Update Minotaur.java

--- Minotaur.java
+++ Minotaur.java
@@ -1,20 +1,20 @@
 package mod;
 
-public class Minotaur {
+public class Minotaur { // this is the minotaur class that stores the information for the minotaur like location
 
-	private int _row, _col;
-	private boolean _isAlive;
+	private int _row, _col; // this initializes the rows and cols
+	private boolean _isAlive; //boolean to check if it is alive
 	
-	public int getRow() { return _row; }
-	public int getCol() { return _col; }
-	public void setPos(int r, int c) { _row = r; _col = c; }
+	public int getRow() { return _row; } // getter method that returns the row position
+	public int getCol() { return _col; } // getter method that returns the col position
+	public void setPos(int r, int c) { _row = r; _col = c; } // setter position that rets the minotaur position
 	
-	public Minotaur(int r, int c) {
+	public Minotaur(int r, int c) { // minotaur constructor that makes the object
 		_row = r;
 		_col = c;
 		_isAlive = true;
 	}
 	
-	public boolean isAlive() { return _isAlive; }
+	public boolean isAlive() { return _isAlive; } //getter method that returns if it is alive
 	// public void kill() {}
 }
